3621e81e53b08c87d6da66fb5d759feee43d2a32
komalrathore1997 <komalrathore70149@gmail.com>
2022-10-17 18:31:03 +0530

    validation data using bean validator

--- src/main/java/com/blog/exception/GlobalExceptionHandaler.java
+++ src/main/java/com/blog/exception/GlobalExceptionHandaler.java
@@ -1,7 +1,12 @@
 package com.blog.exception;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -16,5 +21,19 @@ public class GlobalExceptionHandaler {
 		String message=ex.getMessage();
 		ApiResponse apiResponse=new ApiResponse(message,false);
 		return new ResponseEntity<ApiResponse>(apiResponse,HttpStatus.NOT_FOUND);
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<Map<String,String>> handleMethodArgsNotValidException(MethodArgumentNotValidException ex){
+		Map<String,String>res=new HashMap<>();
+		ex.getBindingResult().getAllErrors().forEach((error)->{
+			String fieldName=((FieldError)error).getField();
+			String Message=error.getDefaultMessage();
+			res.put(fieldName, Message);
+		});
+		
+		return new ResponseEntity<Map<String,String>>(res,HttpStatus.BAD_REQUEST);
+		
+		
 	}
 }
--- src/main/java/com/blog/payloads/UserDto.java
+++ src/main/java/com/blog/payloads/UserDto.java
@@ -3,6 +3,8 @@ package com.blog.payloads;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,13 +16,26 @@ import lombok.Setter;
 public class UserDto {
 	
     private int id;
+    
+    
     @NotEmpty
+    @Size (min=4,message="user name must be min of 4 characters")
     private String name;
-    @Email
+    
+    
+    @Email(message="Email address is not valid!!")
     private String email;
-    @NotNull
+    
+    
+    @NotEmpty
+    @Size(min=3,max=10,message="Password must be min of 3 and max of 10 chars")
     private String password;
-    @NotNull
+   
+    
+    @NotEmpty
     private String about;
+    
+    
+    
 }
  
